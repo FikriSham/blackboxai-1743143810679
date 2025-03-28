33fd95af79721e29589dc15912db507e7c952340
FikriSham <fikri.sham.0@gmail.com>
2025-03-28 14:37:30 +0800

    Add MeerkatConverter/app/src/main/java/com/meerkat/converter/fragments/HistoryFragment.kt 1743143849599

---
+++ MeerkatConverter/app/src/main/java/com/meerkat/converter/fragments/HistoryFragment.kt
@@ -0,0 +1,38 @@
+package com.meerkat.converter.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.meerkat.converter.databinding.FragmentHistoryBinding
+
+class HistoryFragment : Fragment() {
+    private var _binding: FragmentHistoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        binding.historyRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        // TODO: Set up adapter with Room database
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
